1ed3ec5337c7b8575cdd5191eb2d847dc7357b1b
Newton Lilavois <newton@dreamfury.com>
2013-02-05 22:28:25 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Augustblaze::Application.config.secret_token = 'f5438edf6b315e0b500bea5c28d6210026bd3d832be029741c84bccc12644b07aacbab97284293f3bb792cc37c7e67372ae0a06f3b77cc18f23cdfebb1396a88'
